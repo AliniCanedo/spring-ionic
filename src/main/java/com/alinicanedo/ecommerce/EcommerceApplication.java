60e78d87c7a4befa8f403b60235841945ab9baa9
alini <alini.canedo@gmail.com>
2020-09-28 15:13:29 -0300

    Criando operação de instânciação

--- src/main/java/com/alinicanedo/ecommerce/EcommerceApplication.java
+++ src/main/java/com/alinicanedo/ecommerce/EcommerceApplication.java
@@ -1,13 +1,30 @@
 package com.alinicanedo.ecommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceApplication {
+import com.alinicanedo.ecommerce.domain.Categoria;
+import com.alinicanedo.ecommerce.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EcommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escriótorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
