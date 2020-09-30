35ed5846b804a57c3d5e8bcf7d019137b319e8bd
alini <alini.canedo@gmail.com>
2020-09-30 10:33:08 -0300

    Deletando uma Categoria

--- src/main/java/com/alinicanedo/ecommerce/services/CategoriaService.java
+++ src/main/java/com/alinicanedo/ecommerce/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.alinicanedo.ecommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alinicanedo.ecommerce.domain.Categoria;
 import com.alinicanedo.ecommerce.repositories.CategoriaRepository;
+import com.alinicanedo.ecommerce.services.exceptions.DataIntegrityException;
 import com.alinicanedo.ecommerce.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +30,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
