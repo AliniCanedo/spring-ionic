9bc30cfb2c692107119a936e2576f73c7ffacee5
alini <alini.canedo@gmail.com>
2020-10-04 19:07:44 -0300

    Implementando autorização

--- src/main/java/com/alinicanedo/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/alinicanedo/ecommerce/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alinicanedo.ecommerce.security.JWTAuthenticationFilter;
+import com.alinicanedo.ecommerce.security.JWTAuthorizationFilter;
 import com.alinicanedo.ecommerce.security.JWTUtil;
 
 @Configuration
@@ -51,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
